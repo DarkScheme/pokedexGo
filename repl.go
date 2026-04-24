fb34cc1baf149063d25729774a92928908ed3b0b
DarkScheme <paul.informatik@googlemail.com>
2026-04-24 23:56:52 +0200

    added repl function

--- repl.go
+++ repl.go
@@ -1,6 +1,34 @@
 package main
 
-import "strings"
+import (
+	"strings"
+	"fmt"
+	"bufio"
+	"os"
+)
+
+
+func startRepl() {
+	// creates a buffer scanner
+	scanner := bufio.NewScanner(os.Stdin)
+
+	// infinite loop
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := scanner.Text()
+		cleaned := cleanInput(input)
+
+		if len(cleaned) == 0{
+			continue
+		}
+
+		fmt.Print("Your command was: ")
+		fmt.Println(cleaned[0])
+	}
+}
+
+
 
 func cleanInput(text string) []string {
 	words := strings.Fields(strings.ToLower(text))
