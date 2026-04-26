97181df0706106321d1d39a17df1f8244d2d5925
DarkScheme <paul.informatik@googlemail.com>
2026-04-26 23:03:39 +0200

    added map and map-back function to walk the map

--- repl.go
+++ repl.go
@@ -5,15 +5,35 @@ import (
 	"fmt"
 	"bufio"
 	"os"
+	"io"
+	"log"
+	"net/http"
+	"encoding/json"
 )
 
 type cliCommand struct {
 	name string
 	description string
-	callback func() error
+	callback func(*configStruct) error
+}
+
+type configStruct struct {
+	Next string
+	Previous string
+}
+
+type locationAreasResp struct {
+	Count int
+	Next string
+	Previous string
+	Results []struct{
+		Name string
+		URL string
+	}
 }
 
 func getCommands() map[string]cliCommand {
+	// creates the command map
 	newMap := map[string]cliCommand{
 					"exit": {
 					name: "exit",
@@ -25,19 +45,27 @@ func getCommands() map[string]cliCommand {
 						description: "Displays a help message",
 						callback: commandHelp,
 					},
+					"map": {
+						name: "map",
+						description: "Displays the next 20 locations",
+						callback: commandMap,
+					},
+					"mapb": {
+						name: "mapb",
+						description: "Displays the previous 20 locations",
+						callback: commandMapb,
+					},
 				}
 	return newMap
 }
 
 
 func startRepl() {
-	// creates the map
-	
-
-
 
 	// creates a buffer scanner
 	scanner := bufio.NewScanner(os.Stdin)
+
+	cfg := &configStruct{}
 
 	// infinite loop
 	for {
@@ -52,7 +80,7 @@ func startRepl() {
 
 		value, ok := getCommands()[cleaned[0]]
 		if ok {
-			err := value.callback()
+			err := value.callback(cfg)
 			if err != nil {
 				fmt.Println(err)
 			}
@@ -68,19 +96,97 @@ func startRepl() {
 }
 
 // commands here:
-func commandExit() error {
+func commandExit(c *configStruct) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(c *configStruct) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
 	fmt.Println("")
 	for _, newMap := range getCommands() {
 		fmt.Printf("%s: %v\n", newMap.name, newMap.description)
 	}
+	return nil
+}
+
+func commandMap(c *configStruct) error {
+	// this is the get request
+	url := "https://pokeapi.co/api/v2/location-area/"
+	if c.Next != "" {
+		url = c.Next
+	} 
+	res, err := http.Get(url)
+	
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if res.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+	}
+	if err != nil{
+		log.Fatal(err)
+	}
+	// fmt.Printf("%s", body)
+
+	// and this is the Unmarshal JSON to struct
+
+	area := locationAreasResp{}
+	err = json.Unmarshal(body, &area)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	c.Next = area.Next
+	c.Previous = area.Previous
+
+	for _, a := range area.Results {
+		fmt.Println(a.Name)
+	}
+	
+	return nil
+}
+
+func commandMapb(c *configStruct) error {
+	// this is the get request
+	url := "https://pokeapi.co/api/v2/location-area/"
+	if c.Previous != "" {
+		url = c.Previous
+	} 
+	res, err := http.Get(url)
+	
+	if err != nil {
+		log.Fatal(err)
+	}
+	body, err := io.ReadAll(res.Body)
+	res.Body.Close()
+	if res.StatusCode > 299 {
+		log.Fatalf("Response failed with status code: %d and\nbody: %s\n", res.StatusCode, body)
+	}
+	if err != nil{
+		log.Fatal(err)
+	}
+	// fmt.Printf("%s", body)
+
+	// and this is the Unmarshal JSON to struct
+
+	area := locationAreasResp{}
+	err = json.Unmarshal(body, &area)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	c.Next = area.Next
+	c.Previous = area.Previous
+
+	for _, a := range area.Results {
+		fmt.Println(a.Name)
+	}
+	
 	return nil
 }
 
